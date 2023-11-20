8251724515fa6095153008764b9d45a16a688b1a
mToan <mto.work@outlook.com>
2023-11-20 13:39:36 +0100

    pokemon dinamique

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,16 +1,31 @@
 // import styles from './style.css'
 import styles from './styles.module.css'
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
+
 export default function PokemonCard() {
+    let pokemon = pokemonList[1]
     return (
         <figure>
-            <figcaption>
-                Bulbasaur
-            </figcaption>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-                alt="squirtle"
-                // className={styles.cardImg} 
-            />
+            <figcaption>{pokemon.name}</figcaption>
+
+            { pokemon.imgSrc != undefined ?
+                <img src={pokemon.imgSrc}
+                    alt="squirtle"
+                    className={styles.cardImg} 
+                /> 
+                : 
+                "???" 
+            }
         </figure>
     )
 }
